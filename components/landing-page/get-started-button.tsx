76583bdb936cd8d728a5bb2c6d9c59bc46c26d2f
ArvindJayan <jayanarvind@gmail.com>
2025-03-18 09:07:41 +0530

    fix: enhance Get Started button functionality to check user session before navigation

--- components/landing-page/get-started-button.tsx
+++ components/landing-page/get-started-button.tsx
@@ -1,14 +1,30 @@
 "use client";
 
+import { getSession } from "@/app/actions/auth/getSession";
 import { useRouter } from "next/navigation";
+
 
 export default function GetStartedButton() {
     const router = useRouter();
+
+    const handleGetStarted = async () => {
+        try {
+            const { user } = await getSession();
+            if (!user) {
+                return router.push('/login');
+            }
+            router.push('/dashboard');
+        } catch (error) {
+            console.error(error);
+            router.push('/login');
+        }
+    };
+
     return (
         <div>
             <button
                 className="mt-4 px-4 py-2 bg-foreground text-background font-semibold rounded-lg"
-                onClick={() => router.push('/dashboard')}>
+                onClick={handleGetStarted}>
                 Get Started
             </button>
         </div>
