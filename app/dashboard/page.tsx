3017a8cf703da93ee6464cf7abc3254e3eeb5b16
Arvind Jayan <162886201+ArvindJayan@users.noreply.github.com>
2025-03-18 09:48:32 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,4 @@
-import redirect from 'next/navigation'
-import { getSession } from '../actions/auth/getSession'
-
-export default async function DashboardPage() {
-    return (
-        try {
-            const { user } = await getSession();
-            if (!user) {
-            return redirect('/login');
-           }
-        } catch (error) {
-             console.error(error);
-             return redirect('/login');
-        }
-
+export default function DashboardPage() {
         <main className="px-3 py-6">
             <h1 className="text-2xl font-semibold">Dashboard</h1>
         </main>
