6d92aa64a772333f90ed9f6ccaa4b1b728aaaeca
Arvind Jayan <162886201+ArvindJayan@users.noreply.github.com>
2025-03-18 09:38:40 +0530

    refactor: add session check in dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,18 @@
-export default function DashboardPage() {
+import redirect from 'next/navigation'
+import { getSession } from '../actions/auth/getSession'
+
+export default async function DashboardPage() {
     return (
+        try {
+            const { user } = await getSession();
+            if (!user) {
+            return redirect('/login');
+           }
+        } catch (error) {
+             console.error(error);
+             return redirect('/login');
+        }
+
         <main className="px-3 py-6">
             <h1 className="text-2xl font-semibold">Dashboard</h1>
         </main>
