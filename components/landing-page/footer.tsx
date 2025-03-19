cd52c1260e9f92145f41da9ba1542935a8c4c70e
ArvindJayan <jayanarvind@gmail.com>
2025-03-19 22:13:29 +0530

    fix: correct typo in footer section and improve navbar spacing

--- components/landing-page/footer.tsx
+++ components/landing-page/footer.tsx
@@ -12,7 +12,7 @@ const footerSections = [
         title: "Product",
         links: [
             {
-                title: "Feautures",
+                title: "Features",
                 href: "/",
             }
         ],
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
             <NavbarBreadcrumb />
 
             <div className='flex-grow' />
-            <div className='flex items-center justify-end'>
+            <div className='flex items-center space-x-4'>
                 <GetStartedButton />
                 <NavUser />
             </div>
