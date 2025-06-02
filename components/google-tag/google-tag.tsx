3d31ecc55112ce4b8d79a95bd17c4f06a7fb9528
ArvindJayan <jayanarvind@gmail.com>
2025-06-02 21:47:55 +0530

    fix: update Google Tag ID to the correct value

--- components/google-tag/google-tag.tsx
+++ components/google-tag/google-tag.tsx
@@ -3,13 +3,13 @@ export default function GoogleTag() {
         <>
             <title>SentryWeb</title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-M6YMS5RNS1"></script>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-DCFJ82T58Z"></script>
             <script dangerouslySetInnerHTML={{
                 __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-M6YMS5RNS1');
+          gtag('config', 'G-DCFJ82T58Z');
         `,
             }} />
         </>
