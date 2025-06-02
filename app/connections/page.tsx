a3173f9a3052a9c99d055ffc6a8752c60904adbe
Arvind Jayan <162886201+ArvindJayan@users.noreply.github.com>
2025-06-02 13:25:22 +0530

    refactor: add google tag for google analytics

--- app/connections/page.tsx
+++ app/connections/page.tsx
@@ -5,6 +5,17 @@ import { Plus } from "lucide-react";
 export default function ConnectionsPage() {
     return (
         <main className="px-3 py-6 w-full h-full">
+            <Head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-M6YMS5RNS1"></script>
+                <script>
+                  {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-M6YMS5RNS1');
+                  `}
+                </script>
+            </Head>
             <div className="flex justify-between items-center">
                 <h1 className="text-2xl font-semibold mb-4">Connections</h1>
                 <Button className="bg-teal-600 hover:bg-teal-700 font-semibold transition duration-200">Create <Plus /></Button>
