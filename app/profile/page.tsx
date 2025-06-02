52c993626746757b42663ed464ec98402049aa9f
Arvind Jayan <162886201+ArvindJayan@users.noreply.github.com>
2025-06-02 13:26:14 +0530

    refactor: add google tag for google analytics

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,6 +3,17 @@ import UserProfile from "../../components/profile/user-profile";
 export default function Profile() {
     return (
         <main className="w-full h-full mx-5 my-6">
+            <Head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-M6YMS5RNS1"></script>
+                <script>
+                  {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-M6YMS5RNS1');
+                  `}
+                </script>
+              </Head>
             <h1 className="text-2xl font-bold mb-4">Profile</h1>
             <UserProfile />
         </main>
