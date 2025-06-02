6697539e3bddc27641e912da138cdca81a9ddf5b
Arvind Jayan <162886201+ArvindJayan@users.noreply.github.com>
2025-06-02 13:25:48 +0530

    refactor: add google tag for google analytics

--- app/guide/page.tsx
+++ app/guide/page.tsx
@@ -4,6 +4,17 @@ import Guides from "@/components/guide/guides";
 export default function GuidePage() {
     return (
         <main className="flex flex-row mx-5 my-6 w-full h-full text-justify">
+            <Head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-M6YMS5RNS1"></script>
+                <script>
+                  {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-M6YMS5RNS1');
+                  `}
+                </script>
+              </Head>
             <div className="w-3/4 h-3/4 pr-6">
                 <h1 className="text-2xl font-extrabold">Guide</h1>
                 <Guides />
